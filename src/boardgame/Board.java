6410afb755d9996d525b4ac50419c1dad019569e
Guilherme Vargas <guilherme.v@ambevtech.com.br>
2022-09-10 20:31:35 -0300

    [Feat] Adding pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -27,4 +27,17 @@ public class Board {
     public void setColumns(int columns) {
         this.columns = columns;
     }
+
+    public Piece piece(int row, int column) {
+        return pieces[row][column];
+    }
+
+    public Piece piece(Position position){
+        return pieces[position.getRow()][position.getColumn()];
+    }
+
+    public void placePiece(Piece piece, Position position) {
+        pieces[position.getRow()][position.getColumn()] = piece;
+        piece.position = position;
+    }
 }
