c200161a9db69ead5e7831f998ac1a0f491ace9f
Guilherme Vargas <guilherme.v@ambevtech.com.br>
2022-09-10 15:25:25 -0300

    [Feat] Position created

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,11 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Position position = new Position(3, 5);
+        System.out.println(position);
     }
 }
